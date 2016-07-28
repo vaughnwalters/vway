b6ef6dfcddab0e391d2ba4e6804d2f877b278013
Vaughn Walters <vaughnwalters@gmail.com>
2016-07-28 15:28:27 -0500

    logout functioning

--- app/controllers/CitySearchCtrl.js
+++ app/controllers/CitySearchCtrl.js
@@ -1,8 +1,20 @@
 "use strict";
 
-app.controller("CitySearchCtrl", function($scope, $routeParams) {
+app.controller("CitySearchCtrl", function($scope, $routeParams, AuthFactory) {
 
-  console.log("yo");
+
+  $scope.logout = function() {
+    let currentUserId = AuthFactory.getUser();
+    console.log("currentUserId", currentUserId);
+    firebase.auth().signOut()
+    .then(function() {
+      console.log("log out called");
+      currentUserId = AuthFactory.logout();
+      $location.url("/");
+      $scope.$apply();
+      console.log("currentUserId", currentUserId);
+    })
+  };
 
 
 
--- app/controllers/navCtrl.js
+++ app/controllers/navCtrl.js
@@ -1,19 +1,19 @@
 "use strict";
 
-app.controller("NavCtrl", function ($scope) {
-  $scope.navItems = [
-    {
-      name: "Logout",
-      url: "#/logout"
-    },
-    {
-      name: "All Items",
-      url: "#/items/list"
-    },
-    {
-      name: "New Items",
-      url: "#/items/new"
-    }
-  ];
+app.controller("NavCtrl", function ($scope, AuthFactory, $location) {
+ 
+  $scope.logout = function() {
+    let currentUserId = AuthFactory.getUser();
+    console.log("currentUserId", currentUserId);
+    firebase.auth().signOut()
+    .then(function() {
+      console.log("log out called");
+      currentUserId = AuthFactory.logout();
+      $location.url("/");
+      $scope.$apply();
+      console.log("currentUserId", currentUserId);
+    })
+
+  };
 })
 
