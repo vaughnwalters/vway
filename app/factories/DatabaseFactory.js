0564c5f08c6131258b9bf99ab98fb226a88f7415
Vaughn Walters <vaughnwalters@gmail.com>
2016-08-06 11:35:54 -0500

    added ngbind search page

--- app/factories/DatabaseFactory.js
+++ app/factories/DatabaseFactory.js
@@ -5,23 +5,23 @@ app.factory("DatabaseFactory", function($q, $http, FirebaseURL, AuthFactory, Goo
 
 // *************************
 // COMMENT IN TO USE FACTUAL API (also CitySearchCtrl):
-  let getRestaurantList = (searchText) => {
-    console.log("FactualApi", FactualCreds.apiKey);
-    let restaurantArray = []
-    let returnObjArray = null;
-    let count = 0;
-      return $q(function(resolve, reject){
-        $http.get(`http://api.v3.factual.com/t/restaurants-us?filters={"$and":[{"cuisine":{"$includes":"vegan"}}]}&KEY=${FactualCreds.apiKey}&q=${searchText}`
-        )
+  // let getRestaurantList = (searchText) => {
+  //   console.log("FactualApi", FactualCreds.apiKey);
+  //   let restaurantArray = []
+  //   let returnObjArray = null;
+  //   let count = 0;
+  //     return $q(function(resolve, reject){
+  //       $http.get(`http://api.v3.factual.com/t/restaurants-us?filters={"$and":[{"cuisine":{"$includes":"vegan"}}]}&KEY=${FactualCreds.apiKey}&q=${searchText}`
+  //       )
 
 
 // COMMENT IN FOR NASHVILLE TEST DATA (also CitySearchCtrl):
-    // let getRestaurantList = () => { 
-    //     let returnObjArray = null;
-    //     let restaurantArray = [];
-    //     let count = 0;
-    //     return $q(function(resolve, reject){
-    //       $http.get(`nashvilleFactualResponse.json`)
+    let getRestaurantList = () => { 
+        let returnObjArray = null;
+        let restaurantArray = [];
+        let count = 0;
+        return $q(function(resolve, reject){
+          $http.get(`nashvilleFactualResponse.json`)
 // *************************
         .success(function(returnObject){ 
           // push each item into array - will be an object with keyvalue pair 
